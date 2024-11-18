f5a5b3d9ee0045a9ed6b2d3afe5b0d6bcf73f6b7
Pierre Gronnier <pierre.gronnier@gmail.com>
2024-11-18 14:56:04 +0100

    Add human choice and computer choice

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Test");
+const hand = ['rock', 'paper', 'scissors'];
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    return hand[choice];
+}
+
+function getHumanChoice() {
+    const validChoices = ["rock", "paper", "scissors"];
+    let humanChoice;
+
+    while (true) {
+        humanChoice = prompt("Make your choice : rock, paper, scissors ?");
+
+        if (humanChoice === null) {
+            return null; 
+        }
+
+        humanChoice = humanChoice.toLowerCase();
+
+        if (validChoices.includes(humanChoice)) {
+            break;
+        } else {
+            console.error("Error: invalid choice. Please enter rock, paper or scissors.");
+        }
+    }
+
+    return humanChoice;
+}
