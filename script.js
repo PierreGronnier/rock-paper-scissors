6491793b69c1b4c74a860f375d1673ecba09c76e
Pierre Gronnier <pierre.gronnier@gmail.com>
2024-11-18 15:39:27 +0100

    add game logic

--- script.js
+++ script.js
@@ -29,3 +29,48 @@ function getHumanChoice() {
 
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice === humanChoice) {
+        return "It's a tie!";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+}
+
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    for (let round = 0; round < 5; round++) {
+        const humanChoice = getHumanChoice();
+        if (humanChoice === null) {
+            return "Game aborted."; 
+        }
+        const computerChoice = getComputerChoice();
+        const result = playRound(humanChoice, computerChoice);
+        console.log(result);
+        console.log(`Current Score - You: ${humanScore}, Computer: ${computerScore}`);
+    }
+
+    if (humanScore > computerScore) {
+        return `You win the game! Final Score - You: ${humanScore}, Computer: ${computerScore}`;
+    } else if (computerScore > humanScore) {
+        return `You lose the game! Final Score - You: ${humanScore}, Computer: ${computerScore}`;
+    } else {
+        return `The game is a tie! Final Score - You: ${humanScore}, Computer: ${computerScore}`;
+    }
+}
+
+const gameResult = playGame();
+if (gameResult) {
+    console.log(gameResult);
+}
